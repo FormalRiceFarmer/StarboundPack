ee4b953d531406a5ed271babc3feacedf7246432
FormalRiceFarmer <mathiasgirouard@gmail.com>
2014-06-05 10:21:29 -0500

    Starting Fresh!

--- PFC/objects/pfcpump/pfcpump.lua
+++
@@ -1,9 +0,0 @@
-function init(virtual)
-    --self.projectile = entity.configParameter("projectile")
-end
-
-function main(args)
-  if entity.getInboundNodeLevel(0) then
-    world.spawnProjectile("pfc_proj", {entity.position()[1] + 0.5, entity.position()[2]}, entity.id(), {0, 1}, false, {})
-  end
-end
