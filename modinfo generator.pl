50ac5539b8fd64351ae82a656dd60850d21d93c8
FormalRiceFarmer <mathiasgirouard@gmail.com>
2014-06-06 15:11:55 -0500

    script tweak

--- modinfo generator.pl
+++ modinfo generator.pl
@@ -2,9 +2,9 @@ use Cwd;
 opendir(Dir,cwd()) or die "Failure Will Robinson!";
 $dir = cwd();
 
-@files = grep(/\.modpak$/,readdir(Dir));
+@modpaks = grep(/\.modpak$/,readdir(Dir));
 
-foreach (@files) {
+foreach (@modpaks) {
   my $name = substr($_, 0, -7);
   open my $file, ">", "$dir/$name.modinfo" or die "Can't open '$dir/$name.modinfo'\n";
   print $file "
